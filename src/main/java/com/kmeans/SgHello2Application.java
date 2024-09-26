deb3f3d0a7830120afba1c7a1818965c18000130
kmenas00 <abs@afa.com>
2024-09-26 13:50:04 +0900

    sg-hello first commit

---
+++ src/main/java/com/kmeans/SgHello2Application.java
@@ -0,0 +1,13 @@
+package com.kmeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgHello2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgHello2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmeans/SgHello2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgHello2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
